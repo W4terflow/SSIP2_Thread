a5afb79e2ed349da57e92ae3719ca95fb20770b9
W4terflow <ts.greenemteam@gmail.com>
2025-03-21 16:23:14 +0300

    threads 1 again

--- server.py
+++ server.py
@@ -1,34 +1,44 @@
-import socket
-import threading
+import socket  # Модуль для работы с сокетами
+import threading  # Модуль для работы с потоками
 
-
-def handle_client(conn, addr):
-    print(f"Подключен клиент: {addr}")
+def client_handler(conn, addr):
+    # Функция для обработки взаимодействия с клиентом в отдельном потоке
+    print(f"Подключен клиент {addr}")
     msg = ''
-
     while True:
+        # Бесконечный цикл для приема данных от клиента
         data = conn.recv(1024)
+        # Получаем данные размером до 1024 байт
         if not data:
+            # Если данных нет, значит клиент отключился
             break
-        msg += data.decode()
+        msg = data.decode()
+        # Декодируем байтовые данные в строку
+        print(f"Получено сообщение от {addr}: {msg}")
         conn.send(data)
-
-    print(f"Сообщение от клиента {addr}: {msg}")
+        # Отправляем данные обратно клиенту (эхо)
+    print(f"Клиент {addr} отключился")
     conn.close()
-    print(f"Клиент {addr} отключен")
+    # Закрываем соединение с данным клиентом
 
-
-def start_server():
+def main():
     sock = socket.socket()
+    # Создаем сокет (TCP по умолчанию)
     sock.bind(('', 9090))
-    sock.listen(5)
-    print("Сервер запущен и ожидает подключения клиентов...")
-
+    # Связываем сокет с адресом и портом. Пустая строка означает, что сервер будет принят запросы с любых сетевых интерфейсов.
+    sock.listen()
+    # Переводим сокет в режим прослушивания входящих подключений
+    print("Сервер запущен и ожидает подключений...")
     while True:
+        # Бесконечный цикл для принятия новых клиентов
         conn, addr = sock.accept()
-        client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+        # Принимаем новое входящее подключение
+        # conn — новый сокет для обмена данными с клиентом
+        # addr — адрес клиента
+        client_thread = threading.Thread(target=client_handler, args=(conn, addr))
+        # Создаем новый поток для обслуживания клиента
         client_thread.start()
-
+        # Запускаем поток
 
 if __name__ == "__main__":
-    start_server()
+    main()
